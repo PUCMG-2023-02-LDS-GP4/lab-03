9eaf002a7057c944110cd170504227ba6c4e212a
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-11-05 18:34:31 -0300

    Alunos e professores podem registrar uma transação no extrato

--- sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/services/PessoaService.java
+++ sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/services/PessoaService.java
@@ -1,13 +1,18 @@
 package com.unibank.sistemabancario.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.unibank.sistemabancario.models.Extrato;
 import com.unibank.sistemabancario.models.Pessoa;
+import com.unibank.sistemabancario.models.Transacao;
 import com.unibank.sistemabancario.repositories.PessoaRepository;
+import com.unibank.sistemabancario.repositories.TransacaoRepository;
 
 
 @Service
@@ -18,6 +23,9 @@ public class PessoaService {
 
     @Autowired
     private ExtratoService extratoService;
+
+    @Autowired
+    private TransacaoRepository transacaoRepository;
 
     public List<Pessoa> findAll() {
         return pessoaRepository.findAll();
@@ -39,5 +47,22 @@ public class PessoaService {
     public void delete(Long id) {
         pessoaRepository.deleteById(id);
     }
+
+    @Transactional
+    public void registrarTransacao(Pessoa pessoa, int quantidade, String mensagem) {
+        Transacao transacao = new Transacao();
+        transacao.setQuantidade(quantidade);
+        transacao.setMensagem(mensagem);
+        transacao.setData(LocalDateTime.now().toString());
+        
+        Extrato extrato = pessoa.getExtrato();
+        transacao.setExtrato(extrato);
+        
+        transacaoRepository.save(transacao);
+
+        extrato.getListaDeTransacoes().add(transacao);
+        extratoService.saveExtrato(extrato);
+    }
+
 
 }
