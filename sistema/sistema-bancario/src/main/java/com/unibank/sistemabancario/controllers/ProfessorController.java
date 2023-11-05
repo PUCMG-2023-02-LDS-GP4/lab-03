1061838acd23bba824d6e94b4e4714af2cad4488
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-11-05 18:48:17 -0300

    Controller de professor para ele poder enviar moedas

---
+++ sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/controllers/ProfessorController.java
@@ -0,0 +1,33 @@
+package com.unibank.sistemabancario.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.unibank.sistemabancario.services.ProfessorService;
+
+@RestController
+@RequestMapping("/api/professores")
+public class ProfessorController {
+
+    @Autowired
+    private ProfessorService professorService;
+
+    @PostMapping("/{professorId}/enviarMoedas/{alunoId}")
+    public ResponseEntity<?> enviarMoedas(
+            @PathVariable Long professorId,
+            @PathVariable Long alunoId,
+            @RequestParam int quantidade) {
+                
+        try {
+
+            professorService.enviarMoedas(professorId, alunoId, quantidade);
+            return ResponseEntity.ok().body("Moedas enviadas com sucesso.");
+
+        } catch (RuntimeException e) {
+
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
