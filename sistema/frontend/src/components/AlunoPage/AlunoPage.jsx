7ace812b5fae02ad6a568828ee019522c1d796f4
Gabriel Estevão <gensobrinho@hotmail.com>
2023-11-06 10:19:17 -0300

    feature: tela de alunos criada

---
+++ sistema/frontend/src/components/AlunoPage/AlunoPage.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header/Header"
+
+export default function AlunoPage() {
+
+  return (
+    <>
+        <Header></Header>
+    </>
+  )
+}
---
+++ sistema/frontend/src/components/AlunoPage/Header/Header.jsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Profile from '../Profile/Profile';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Header() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Perfil" {...a11yProps(0)} />
+          <Tab label="Extrato" {...a11yProps(1)} />
+          <Tab label="Vantagens" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Profile />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <p>1</p>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <p>2</p>
+      </CustomTabPanel>
+    </Box>
+  );
+}
