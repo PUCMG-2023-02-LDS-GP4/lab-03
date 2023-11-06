086dea82140bc5578486cffe3d9b34920b0e5f63
Baldiotti <111604631+Baldiotti@users.noreply.github.com>
2023-11-06 09:29:11 -0300

    feat: Login funcionando

--- sistema/frontend/src/components/LoginPage/Login/styles.js
+++ sistema/frontend/src/components/LoginPage/Login/styles.js
@@ -1,49 +1,59 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 10px;
-  height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	gap: 10px;
+	height: 100vh;
 `;
 
-export const Content = styled.div`
-  gap: 15px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  box-shadow: 0 1px 2px #0003;
-  background-color: white;
-  max-width: 350px;
-  padding: 20px;
-  border-radius: 5px;
+export const Content = styled.form`
+	gap: 15px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	width: 100%;
+	box-shadow: 0 1px 2px #0003;
+	background-color: white;
+	max-width: 350px;
+	padding: 20px;
+	border-radius: 5px;
+`;
+
+export const Input = styled.input`
+	outline: none;
+	padding: 16px 20px;
+	width: 100%;
+	border-radius: 5px;
+	font-size: 16px;
+	background-color: #f0f2f5;
+	border: none;
 `;
 
 export const Label = styled.label`
-  font-size: 18px;
-  font-weight: 600;
-  color: #676767;
+	font-size: 18px;
+	font-weight: 600;
+	color: #676767;
 `;
 
 export const LabelSignup = styled.label`
-  font-size: 16px;
-  color: #676767;
+	font-size: 16px;
+	color: #676767;
 `;
 
 export const labelError = styled.label`
-  font-size: 14px;
-  color: red;
+	font-size: 14px;
+	color: red;
 `;
 
 export const Strong = styled.strong`
-  cursor: pointer;
+	cursor: pointer;
 
-  a {
-    text-decoration: none;
-    color: #676767;
-  }
+	a {
+		text-decoration: none;
+		color: #676767;
+	}
 `;
--- sistema/frontend/src/routes/router.jsx
+++ sistema/frontend/src/routes/router.jsx
@@ -2,13 +2,14 @@ import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../components/LoginPage/Login/Login";
 import Cadastro from "../components/Cadastro/Cadastro";
+import AdminPage from "../components/AdminPage/AdminPage";
 
 const RoutesApp = () => {
 	return (
 		<BrowserRouter>
 			<Fragment>
 				<Routes>
-					<Route exact path='/home' element={<h1>Logado</h1>} />
+					<Route exact path='/home' element={<AdminPage />} />
 					<Route path='/' element={<Login />} />
 					<Route exact path='/cadastro' element={<Cadastro />} />
 					<Route path='*' element={<Login />} />
