4c204dee44504bc77bf7ed1cbbf001bac2d0c44e
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-11-10 20:29:11 -0300

    Adição de cursos

---
+++ sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/models/enums/Cursos.java
@@ -0,0 +1,13 @@
+package com.unibank.sistemabancario.models.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Cursos {
+    MATEMATICA,
+    MEDICINA,
+    DIREITO,
+    BIOLOGIA,
+    SOCIOLOGIA
+
+}
