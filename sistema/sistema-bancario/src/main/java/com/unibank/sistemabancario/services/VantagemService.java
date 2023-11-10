560422da5891738a14f33cfbeabf1ba56a56e541
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-11-10 20:28:57 -0300

    fix no getEmpresa de vantagem

--- sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/services/VantagemService.java
+++ sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/services/VantagemService.java
@@ -1,10 +1,13 @@
 package com.unibank.sistemabancario.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
+import com.unibank.sistemabancario.models.Empresa;
 import com.unibank.sistemabancario.models.Vantagem;
 import com.unibank.sistemabancario.models.dtos.CreateVantagemDTO;
+import com.unibank.sistemabancario.repositories.EmpresaRepository;
 import com.unibank.sistemabancario.repositories.VantagemRepository;
 
 @Service
@@ -13,14 +16,19 @@ public class VantagemService {
     @Autowired
     private VantagemRepository vantagemRepository;
 
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
     public Vantagem createVantagem(CreateVantagemDTO createVantagemDTO) {
         Vantagem vantagem = new Vantagem();
         vantagem.setDescricao(createVantagemDTO.getDescricao());
         vantagem.setCustoEmMoedas(createVantagemDTO.getCustoEmMoedas());
         vantagem.setFoto(createVantagemDTO.getFoto());
 
-        vantagem.setEmpresa(createVantagemDTO.getEmpresa());
+        Empresa empresa = empresaRepository.findById(createVantagemDTO.getEmpresaId())
+        .orElseThrow(() -> new RuntimeException("Empresa not found"));
 
+        vantagem.setEmpresa(empresa);
         return vantagemRepository.save(vantagem);
     }
 }
