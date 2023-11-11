43bcaf8db6129bbed1e4a1b2d53a7e943e76b334
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-11-11 20:34:59 -0300

    Update InstituicaoRepository.java

--- sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/repositories/InstituicaoRepository.java
+++ sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/repositories/InstituicaoRepository.java
@@ -1,9 +1,12 @@
 package com.unibank.sistemabancario.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.unibank.sistemabancario.models.Instituicao;
 
-public interface InstituicaoRepository extends JpaRepository<Instituicao, Long>{
+public interface InstituicaoRepository extends JpaRepository<Instituicao, Long>, CustomInstituicaoRepository{
     
+    Optional<Instituicao> findById(Long id);
 }
