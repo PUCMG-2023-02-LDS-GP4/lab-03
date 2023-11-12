ff714c5079454c301692f148824c4f0ccce813a5
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-11-12 14:42:41 -0300

    remoção da entidade Instituicao

--- sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/repositories/CustomInstituicaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unibank.sistemabancario.repositories;
-
-import com.unibank.sistemabancario.models.Instituicao;
-
-public interface CustomInstituicaoRepository {
-    Instituicao merge(Instituicao instituicao);
-}
