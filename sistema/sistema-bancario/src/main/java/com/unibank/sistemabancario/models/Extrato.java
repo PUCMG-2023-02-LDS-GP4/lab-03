af403372f369feb9b224320c3d508414b060eb00
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-11-12 20:14:49 -0300

    Update Extrato.java

--- sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/models/Extrato.java
+++ sistema/sistema-bancario/src/main/java/com/unibank/sistemabancario/models/Extrato.java
@@ -3,7 +3,6 @@ package com.unibank.sistemabancario.models;
 import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -21,7 +20,6 @@ public class Extrato {
     @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "pessoa_id")
     @JsonBackReference
-    @JsonIgnore
     private Pessoa pessoa;
 
     @OneToMany(mappedBy = "extrato")
