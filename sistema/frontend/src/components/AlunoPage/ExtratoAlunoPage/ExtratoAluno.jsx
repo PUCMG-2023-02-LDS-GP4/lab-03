7a77ce5b178d71e3d474da6c4f8d063692193d1c
luanaf4 <luana.fleury4@gmail.com>
2023-11-11 23:25:23 -0300

    Extrato Aluno

--- sistema/frontend/src/components/AlunoPage/ExtratoAlunoPage/ExtratoAluno.jsx
+++ sistema/frontend/src/components/AlunoPage/ExtratoAlunoPage/ExtratoAluno.jsx
@@ -1,10 +1,4 @@
 import React from 'react'
-import { useEffect, useState } from "react";
-import Table from "../../Table/Table";
-import { TableRow, TableCell, Button } from "@mui/material";
-import CustomModal from "../../CustomModal/CustomModal";
-import AlunoCreateForm from "./AlunoCreateForm";
-import AlunoUpdateForm from "./AlunoUpdateForm";
 
 export default function ExtratoAluno() {
   return (
